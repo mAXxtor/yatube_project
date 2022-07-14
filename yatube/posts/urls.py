8294b72ed98473505fff1084e22300fcad8680be
Максим Вербицкий <maxxtor21@gmail.com>
2022-07-15 02:58:37 +0400

    Создание роутинга в приложении posts шаблона group_posts

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,4 +1,4 @@
-"""yatube URL Configuration
+"""posts URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    # Подключаем файл urls приложения posts
-    path('', include('posts.urls')),
-    path('admin/', admin.site.urls),
+    # Главная страница
+    path('', views.index),
+    # Страницы соообществ
+    path('group/<slug:slug>/', views.group_posts),
 ]
