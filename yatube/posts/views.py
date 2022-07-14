a412845e2796f4a64d260acf8492bbd3d3fe9b21
Максим Вербицкий <maxxtor21@gmail.com>
2022-07-15 03:34:15 +0400

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+# Главная страница.
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+# Страница с постами, отфильтрованными по группам.
+def group_posts(request, slug):
+    return HttpResponse(f'Здесь будут посты, отфильтрованные по группам. Например сейчас запрос был по группе {slug}')
