a5d74c75c1068af5ffa08151bc325086941e8a92
Максим Вербицкий <maxxtor21@gmail.com>
2022-08-11 00:27:26 +0400

    Добавлены profile и post_detail

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,13 @@
+from django.conf import settings
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 
-from .models import Group, Post
+from .models import Group, Post, User
 
 
 def index(request):
     posts = Post.objects.all()
-    paginator = Paginator(posts, 10)
+    paginator = Paginator(posts, settings.NUMBER_OF_POSTS)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {'page_obj': page_obj}
@@ -16,7 +17,7 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.all()
-    paginator = Paginator(posts, 10)
+    paginator = Paginator(posts, settings.NUMBER_OF_POSTS)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
@@ -25,3 +26,25 @@ def group_posts(request, slug):
         'page_obj': page_obj
     }
     return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, author):
+    author = get_object_or_404(User, username=author)
+    profile_posts = Post.objects.filter(author=author)
+    paginator = Paginator(profile_posts, settings.NUMBER_OF_POSTS)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'author': author,
+        'profile_posts': profile_posts,
+        'page_obj': page_obj
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = {
+        'post': post,
+    }
+    return render(request, 'posts/post_detail.html', context)
